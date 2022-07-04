e9b00e09ee0d40f34cb6ea85fd8e2689da420fec
Ariel <ariel.alvear.l@gmail.com>
2022-07-04 13:38:26 -0400

    RaceParticipants: Nested form added to create league_race with their race_participants, with positions and points.

--- app/controllers/league_races_controller.rb
+++ app/controllers/league_races_controller.rb
@@ -13,7 +13,9 @@ class LeagueRacesController < ApplicationController
 
   # GET /league_races/new
   def new
+    @league = League.find(params["league_id"])
     @league_race = LeagueRace.new
+    @positions_for_select = PointsForPosition.from_score_system(@league.score_system.id)
   end
 
   # GET /league_races/1/edit
@@ -23,10 +25,18 @@ class LeagueRacesController < ApplicationController
   # POST /league_races or /league_races.json
   def create
     @league_race = LeagueRace.new(league_race_params)
+    @points_for_position = PointsForPosition.from_score_system(params[:score_system_id].to_i)
+    @league_race.race_participants.each do |participant|
+      if participant.position.to_i > 20
+        participant.score = 0
+      else
+        participant.score = @points_for_position.find_by(position: participant.position).points
+      end
+    end
 
     respond_to do |format|
       if @league_race.save
-        format.html { redirect_to league_race_url(@league_race), notice: "League race was successfully created." }
+        format.html { redirect_to race_participants_path(league_race_id: @league_race.id), notice: "Carrera creada correctamente" }
         format.json { render :show, status: :created, location: @league_race }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -58,6 +68,9 @@ class LeagueRacesController < ApplicationController
     end
   end
 
+  def score_for_position(score_system)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_league_race
@@ -66,6 +79,6 @@ class LeagueRacesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def league_race_params
-      params.require(:league_race).permit(:name, :description, :race_track_id, :league_id)
+      params.require(:league_race).permit(:name, :description, :race_track_id, :league_id, race_participants_attributes: [:id, :user_id, :race_league_id, :position, :score, :status, :_destroy])
     end
 end
--- app/models/league_race.rb
+++ app/models/league_race.rb
@@ -1,6 +1,8 @@
 class LeagueRace < ApplicationRecord
   belongs_to :race_track
   belongs_to :league
+  has_many :race_participants
+  accepts_nested_attributes_for :race_participants, allow_destroy: true
 
   scope :from_league, -> (league) { where(league_id: league) }
 end
